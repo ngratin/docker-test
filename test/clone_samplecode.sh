9be9f476c4d990e60e517ee160e099e6ff138bdd
Masafumi Noguchi <ngratin@gmail.com>
2017-08-24 16:09:10 +0900

    test

--- test/clone_samplecode.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-
-git clone https://github.com/mobingilabs/default-site-php test/code
--- test/push_image_to_hub.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-set -e
-
-curl -H "Content-Type: application/json" --data '{"build": true}' -X POST https://registry.hub.docker.com/u/ngratin/docker-test/trigger/${DOCKER_BUILD_TOKEN}/
