e1d6fffb58d89374210ee1f49e8051e70ef2e78b
Masafumi Noguchi <ngratin@gmail.com>
2017-09-04 17:13:40 +0900

    Revert "test"
    This reverts commit 71d2c539b1c630c0788dea54b844e61ad7927613.

--- run.sh
+++ run.sh
@@ -2,18 +2,10 @@
 
 echo "installing" > /var/log/container_status
 
-# setup webserver doc roots
-DOCROOT=$(echo $WEBSERVER_DOCUMENT_ROOT | sed 's/[]\/$*.^|[]/\\&/g')
-sed -i -- "s/##WEBSERVER_DOCUMENT_ROOT##/$DOCROOT/g" /etc/nginx/sites-available/default
-
-# Clean double slashes
-sed -i -- 's/www\/\//www\//g' /etc/nginx/sites-available/default
-
-echo "Running init script"
-bash /tmp/init/init.sh >> /var/log/startup.log 2>&1
+bash /tmp/init/init.sh >>/var/log/startup.log 2>&1
 
 echo "complete" > /var/log/container_status
 
 mkdir /var/log/supervisor /var/log/nginx
 
-/usr/bin/supervisord
+exec /usr/bin/supervisord
